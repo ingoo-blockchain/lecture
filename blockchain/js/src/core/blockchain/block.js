6687a9133de7d85bb5bc432ef2548ce06d622c48
ivy <jihyunivy@gmailcom>
2022-12-21 17:57:14 +0900

    block, chain 주석 추가

--- blockchain/js/src/core/blockchain/block.js
+++ blockchain/js/src/core/blockchain/block.js
@@ -1,129 +1,159 @@
-const IBlock = require('./interfaces/block.interface')
+const IBlock = require("./interfaces/block.interface");
 class Block extends IBlock {
-    #merkle
-    #SHA256
-    #hexToBinary
+	#merkle;
+	#SHA256;
+	#hexToBinary;
 
-    static difficulty_adjustment_interval
-    static block_generation_interval
-    static unit
+	static difficulty_adjustment_interval;
+	static block_generation_interval;
+	static unit;
 
-    constructor({
-        difficulty_adjustment_interval,
-        block_generation_interval,
-        unit,
-        merkle,
-        SHA256,
-        hexToBinary,
-    }) {
-        super()
+	constructor({
+		difficulty_adjustment_interval,
+		block_generation_interval,
+		unit,
+		merkle,
+		SHA256,
+		hexToBinary,
+	}) {
+		super();
 
-        this.#merkle = merkle
-        this.#SHA256 = SHA256
-        this.#hexToBinary = hexToBinary
+		this.#merkle = merkle;
+		this.#SHA256 = SHA256;
+		this.#hexToBinary = hexToBinary;
 
-        Block.DIFFICULTY_ADJUSTMENT_INTERVAL = difficulty_adjustment_interval
-        Block.BLOCK_GENERATION_INTERVAL = block_generation_interval
-        Block.UNIT = unit
-    }
+		Block.DIFFICULTY_ADJUSTMENT_INTERVAL = difficulty_adjustment_interval;
+		Block.BLOCK_GENERATION_INTERVAL = block_generation_interval;
+		Block.UNIT = unit;
+	}
 
-    create({ previousBlock, adjustmentDifficulty, adjustmentTimestamp, data }) {
-        try {
-            const { height, hash: previousHash } = previousBlock
-            this.height = height + 1
-            this.previousHash = previousHash
-            const merkleRoot = this.getMerkleRoot(data)
+	/// 이전 블록과 조정난이도, 조정타임스탬프를 가지고 현재 블록을 생성한 후 nonce값을 조정해가며 블록 업데이트
+	/// nonce값이 조정된 새로운 블록의 유효성을 검증한 후 유효한 경우 생성된 블록 반환
+	create({ previousBlock, adjustmentDifficulty, adjustmentTimestamp, data }) {
+		try {
+			const { height, hash: previousHash } = previousBlock;
+			this.height = height + 1;
+			this.previousHash = previousHash;
+			const merkleRoot = this.getMerkleRoot(data);
 
-            if (merkleRoot.isError) throw new Error(merkleRoot.error)
-            this.merkleRoot = merkleRoot.value
+			if (merkleRoot.isError) throw new Error(merkleRoot.error);
+			this.merkleRoot = merkleRoot.value;
 
-            this.nonce = 0
-            this.timestamp = new Date().getTime()
-            this.difficulty = this.getDifficulty({
-                height: this.height,
-                timestamp: this.timestamp,
-                previousDifficulty: previousBlock.difficulty,
-                adjustmentDifficulty,
-                adjustmentTimestamp,
-            })
+			this.nonce = 0;
+			this.timestamp = new Date().getTime();
+			this.difficulty = this.getDifficulty({
+				height: this.height,
+				timestamp: this.timestamp,
+				previousDifficulty: previousBlock.difficulty,
+				adjustmentDifficulty,
+				adjustmentTimestamp,
+			});
 
-            this.hash = this.createBlockHash(this)
-            this.data = data
-            this.updateBlock({ previousBlock, adjustmentDifficulty, adjustmentTimestamp })
+			this.hash = this.createBlockHash(this);
+			this.data = data;
+			this.updateBlock({
+				previousBlock,
+				adjustmentDifficulty,
+				adjustmentTimestamp,
+			});
 
-            const isValid = this.isValidNewBlock({ previousBlock, newBlock: this })
-            if (isValid.isError) throw new Error(isValid.error)
-            return this
-        } catch (e) {
-            throw new Error(e.message)
-        }
-    }
+			const isValid = this.isValidNewBlock({
+				previousBlock,
+				newBlock: this,
+			});
+			if (isValid.isError) throw new Error(isValid.error);
+			return this;
+		} catch (e) {
+			throw new Error(e.message);
+		}
+	}
 
-    updateBlock() {
-        let hashBinary = this.hashToBinary(this.hash)
+	/// 2진수로 변환된 해시값이 0으로 시작하지 않을 경우 nonce 값을 1씩 추가하는 함수
+	updateBlock() {
+		let hashBinary = this.hashToBinary(this.hash);
 
-        while (!hashBinary.startsWith('0'.repeat(this.difficulty))) {
-            this.nonce += 1
-            this.timestamp = new Date().getTime()
-            this.difficulty = this.getDifficulty({
-                height: this.height,
-                timestamp: this.timestamp,
-                previousDifficulty: previousBlock.difficulty,
-                adjustmentDifficulty,
-                adjustmentTimestamp,
-            })
-            this.hash = this.createBlockHash(this)
-            hashBinary = hexToBinary(this.hash)
-        }
-    }
+		while (!hashBinary.startsWith("0".repeat(this.difficulty))) {
+			this.nonce += 1;
+			this.timestamp = new Date().getTime();
+			this.difficulty = this.getDifficulty({
+				height: this.height,
+				timestamp: this.timestamp,
+				previousDifficulty: previousBlock.difficulty,
+				adjustmentDifficulty,
+				adjustmentTimestamp,
+			});
+			this.hash = this.createBlockHash(this);
+			hashBinary = hexToBinary(this.hash);
+		}
+	}
 
-    hashToBinary(hash) {
-        return this.#hexToBinary(hash)
-    }
+	/// 16진수의 해시를 에서 2진수로 변환하여 반환하는 함수
+	hashToBinary(hash) {
+		return this.#hexToBinary(hash);
+	}
 
-    createBlockHash(block) {
-        return this.#SHA256(
-            Object.entries(block)
-                .filter((v) => v[0] !== 'hash' && v[0] !== 'data')
-                .join(''),
-        ).toString()
-    }
+	/// 블록 해시를 반환하는 함수
+	createBlockHash(block) {
+		return this.#SHA256(
+			Object.entries(block)
+				.filter((v) => v[0] !== "hash" && v[0] !== "data")
+				.join("")
+		).toString();
+	}
 
-    getMerkleRoot(data) {
-        return Array.isArray(data)
-            ? { isError: false, value: this.#merkle('sha256').sync(data).root() || '0'.repeat(64) }
-            : { isError: true, error: 'The content of data is not an array.' }
-    }
+	/// 블록의 바디 부분인 data를 가지고 32바이트의 해시를 반환하는 함수
+	getMerkleRoot(data) {
+		return Array.isArray(data)
+			? {
+					isError: false,
+					value:
+						this.#merkle("sha256").sync(data).root() ||
+						"0".repeat(64),
+			  }
+			: { isError: true, error: "The content of data is not an array." };
+	}
 
-    getDifficulty({
-        height,
-        timestamp,
-        adjustmentDifficulty,
-        adjustmentTimestamp,
-        previousDifficulty,
-    }) {
-        if (height < 10) return 0
-        if (height >= 10 && height < 20) return 1
-        if (height % Block.DIFFICULTY_ADJUSTMENT_INTERVAL !== 0) return previousDifficulty
+	/// 함수의 반환 값 시나리오
+	/// 1. 블록의 높이가 10보다 작은 경우 난이도 0.
+	/// 2. 블록의 높이가 10~19사이 인 경우 난이도 1을 반환
+	/// 3. 블록의 높이에서 난이도를 조정하기 위한 단위값을 나눈 나머지가 0이 아닌 경우 이전의 난이도를 반환
+	/// 4. 1~3에 해당하지 않는 경우에는 소요된 시간과 예상된 시간을 비교하여 조정된 난이도를 반환
+	getDifficulty({
+		height,
+		timestamp,
+		adjustmentDifficulty,
+		adjustmentTimestamp,
+		previousDifficulty,
+	}) {
+		if (height < 10) return 0;
+		if (height >= 10 && height < 20) return 1;
+		if (height % Block.DIFFICULTY_ADJUSTMENT_INTERVAL !== 0)
+			return previousDifficulty;
 
-        const timeTaken = timestamp - adjustmentTimestamp
-        const timeExpected =
-            Block.UNIT * Block.BLOCK_GENERATION_INTERVAL * Block.DIFFICULTY_ADJUSTMENT_INTERVAL
+		const timeTaken = timestamp - adjustmentTimestamp;
+		const timeExpected =
+			Block.UNIT *
+			Block.BLOCK_GENERATION_INTERVAL *
+			Block.DIFFICULTY_ADJUSTMENT_INTERVAL;
 
-        if (timeTaken < timeExpected / 2) return adjustmentDifficulty + 1
-        if (timeTaken > timeExpected * 2) return adjustmentDifficulty - 1
-        return adjustmentDifficulty
-    }
+		if (timeTaken < timeExpected / 2) return adjustmentDifficulty + 1;
+		if (timeTaken > timeExpected * 2) return adjustmentDifficulty - 1;
+		return adjustmentDifficulty;
+	}
 
-    isValidNewBlock({ previousBlock, newBlock }) {
-        if (previousBlock.height + 1 !== newBlock.height)
-            return { isError: true, error: 'Block height is incorrect.' }
-        if (previousBlock.hash !== newBlock.previousHash)
-            return { isError: true, error: 'The previous hash value is not correct.' }
-        if (this.createBlockHash(newBlock) !== newBlock.hash)
-            return { isError: true, error: 'Block hash is incorrect.' }
-        return { isError: false, value: newBlock }
-    }
+	/// 블록의 높이, 이전 블록의 해시, 새로운 블록의 해쉬를 비교하여 유효한 블록인 지를 검증하는 함수
+	isValidNewBlock({ previousBlock, newBlock }) {
+		if (previousBlock.height + 1 !== newBlock.height)
+			return { isError: true, error: "Block height is incorrect." };
+		if (previousBlock.hash !== newBlock.previousHash)
+			return {
+				isError: true,
+				error: "The previous hash value is not correct.",
+			};
+		if (this.createBlockHash(newBlock) !== newBlock.hash)
+			return { isError: true, error: "Block hash is incorrect." };
+		return { isError: false, value: newBlock };
+	}
 }
 
-module.exports = Block
+module.exports = Block;
--- blockchain/js/src/core/blockchain/chain.js
+++ blockchain/js/src/core/blockchain/chain.js
@@ -1,100 +1,105 @@
-const IBlock = require('./interfaces/block.interface')
+const IBlock = require("./interfaces/block.interface");
 
 class Chain extends Array {
-    #block
-    #difficultyAdjustmentInterval
+	#block;
+	#difficultyAdjustmentInterval;
 
-    /**
-     * Initializes a new instance of the Chain class.
-     *
-     * @param {Object} params
-     * @param {IBlock} params.block
-     * @param {IBlock} params.genesis
-     * @param {number} params.difficultyAdjustmentInterval
-     */
-    constructor({ block, genesis, difficulty_adjustment_interval }) {
-        super()
-        this.#block = block
-        this.#difficultyAdjustmentInterval = difficulty_adjustment_interval
-        this.push(genesis)
-    }
+	/**
+	 * Initializes a new instance of the Chain class.
+	 *
+	 * @param {Object} params
+	 * @param {IBlock} params.block
+	 * @param {IBlock} params.genesis
+	 * @param {number} params.difficultyAdjustmentInterval
+	 */
+	constructor({ block, genesis, difficulty_adjustment_interval }) {
+		super();
+		this.#block = block;
+		this.#difficultyAdjustmentInterval = difficulty_adjustment_interval;
+		this.push(genesis);
+	}
 
-    get block() {
-        return this.#block
-    }
+	get block() {
+		return this.#block;
+	}
 
-    get difficultyAdjustmentInterval() {
-        return this.#difficultyAdjustmentInterval
-    }
+	get difficultyAdjustmentInterval() {
+		return this.#difficultyAdjustmentInterval;
+	}
 
-    get latestBlock() {
-        return this[this.length - 1]
-    }
+	get latestBlock() {
+		return this[this.length - 1];
+	}
 
-    get adjustmentBlock() {
-        const length = this.length
-        const interval = length - this.#difficultyAdjustmentInterval
-        return interval < 0 ? this[0] : this[interval]
-    }
+	get adjustmentBlock() {
+		const length = this.length;
+		const interval = length - this.#difficultyAdjustmentInterval;
+		return interval < 0 ? this[0] : this[interval];
+	}
 
-    /**
-     * Adds a new block to the end of the chain.
-     *
-     * @param {string[]} data
-     * @returns {IBlock}
-     */
-    addBlock(data) {
-        try {
-            const previousBlock = this.latestBlock
-            const adjustmentBlock = this.adjustmentBlock
-            const newBlock = this.#block.create({
-                previousBlock,
-                adjustmentDifficulty: adjustmentBlock.difficulty,
-                adjustmentTimestamp: adjustmentBlock.timestamp,
-                data,
-            })
+	/**
+	 * Adds a new block to the end of the chain.
+	 *
+	 * @param {string[]} data
+	 * @returns {IBlock}
+	 */
 
-            this.addToChain(newBlock)
-            return newBlock
-        } catch (e) {
-            console.error(e)
-            return null
-        }
-    }
+	/// 블록 생성 후 체인에 추가하는 함수
+	addBlock(data) {
+		try {
+			const previousBlock = this.latestBlock;
+			const adjustmentBlock = this.adjustmentBlock;
+			const newBlock = this.#block.create({
+				previousBlock,
+				adjustmentDifficulty: adjustmentBlock.difficulty,
+				adjustmentTimestamp: adjustmentBlock.timestamp,
+				data,
+			});
 
-    /**
-     * @param {IBlock} receviedBlock
-     * @returns {Chain[] || null}
-     */
-    addToChain(receviedBlock) {
-        try {
-            const newBlock = this.#block.isValidNewBlock({
-                newBlock: receviedBlock,
-                previousBlock: this.latestBlock,
-            })
+			this.addToChain(newBlock);
+			return newBlock;
+		} catch (e) {
+			console.error(e);
+			return null;
+		}
+	}
 
-            if (newBlock.isError) throw new Error(newBlock.error)
-            this.push(newBlock.value)
+	/// 유효성을 검증한 새로운 블럭을 체인 배열에 push하여 체인에 추가하는 함수
+	/**
+	 * @param {IBlock} receviedBlock
+	 * @returns {Chain[] || null}
+	 */
+	addToChain(receviedBlock) {
+		try {
+			const newBlock = this.#block.isValidNewBlock({
+				newBlock: receviedBlock,
+				previousBlock: this.latestBlock,
+			});
 
-            return this
-        } catch (e) {
-            console.error(e.meesage)
-        }
-    }
+			if (newBlock.isError) throw new Error(newBlock.error);
+			this.push(newBlock.value);
 
-    /**
-     * @param {IBlock[]} chain
-     * @returns {{ isError: false; value: any } || { isError: true; error: string }}
-     */
-    isValidChain(chain) {
-        //TODO: implement this method
-        return true
-    }
+			return this;
+		} catch (e) {
+			console.error(e.meesage);
+		}
+	}
 
-    replaceChain(chain) {
-        //TODO: implement this method
-        return true
-    }
+	/// 체인의 유효성을 검증하는 함수
+	/**
+	 * @param {IBlock[]} chain
+	 * @returns {{ isError: false; value: any } || { isError: true; error: string }}
+	 */
+	isValidChain(chain) {
+		//TODO: implement this method
+		return true;
+	}
+
+	/// 유효하지 않은 체인을 유효한 체인으로 대체하는 함수
+	replaceChain(chain) {
+		//TODO: implement this method
+		return true;
+	}
 }
 
-module.exports = Chain
+module.exports = Chain;
