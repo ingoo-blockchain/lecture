e5f6712c1219607ff7f7bd5f2489540773d02a3d
ingoo <web7722@gmail.com>
2023-04-11 11:37:04 +0900

    feat:server.js 에서 serverless-http 내용 추가

--- deploy/src/lambda/server.js
+++ deploy/src/lambda/server.js
@@ -1,3 +1,4 @@
+const serverless = require('serverless-http')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,6 +11,9 @@ app.get('/test', (req, res) => {
     res.send('test')
 })
 
-app.listen(port, () => {
-    console.log(`server is running on port : ${port}`)
-})
+// express 에서 직접 실행 X
+// app.listen(port, () => {
+//     console.log(`server is running on port : ${port}`)
+// })
+
+module.exports.handler = serverless(app)
